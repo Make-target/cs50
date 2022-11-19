befbee283a5c153a10ade45f4b5429b693dffaae
Make-target <115178414+7xirtaM7@users.noreply.github.com>
2022-11-19 21:33:48 +0000

    Add finished credit.c, ready to go I rewrite the program using new solutions and more loops to reduce unnecessary code. Also, the program can now check a card provider and get input from a user to get the credit card number.

--- credit.c
+++ credit.c
@@ -1,69 +1,85 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int modulo(int x);
+unsigned long american_express_15 = 100000000000000;
+unsigned long master_card_or_visa_16 = 1000000000000000;
+unsigned long visa_13 = 1000000000000;
+unsigned long user_card = 0;
+unsigned long card = 0;
+int first_digit = 0;
+int second_digit = 0;
+int digit = 0;
+int modulo = 10;
 
-int main(void)
+int main (void)
 {
-    int x = 0;
-    modulo(x);
-}
 
-int modulo(int x)
-{
-    long card_number = 101213141516171819;
-    for (int i = 0; i < 0; i++)
+    user_card = get_long ("\n\n\n\n\n\nCard number: ");
+    card = user_card;
+    while (card != 0)
     {
-        int every_first_digit = 0;
-        int every_second_digit = 0;
+        digit = card % modulo; // Search for the last digit of card number
+        first_digit += digit; // Add last digit to a pot
+        card *= 0.10;         // Move to another digit by subtracting one zero
 
-        x = card_number % 10;
-        card_number -= x;
-        if (every_second_digit > 9)
+        digit = card % modulo;
+        if (digit * 2 > 9) // Separate and add numbers greater than 9(after multiplying) as in Lunh's Algorithm
         {
-            while(every_second_digit > 10)
-            {
-                every_second_digit *= 0.10;
-            }
+            digit = digit * 2;
+            int x = digit;
+            x = digit % 10;
+            second_digit += x;
+            x = digit * 0.10;
+            second_digit += x;
+            card *= 0.10;
+
         }
-        printf (" !hi, %ld! ", card_number);
+
+        else // Multiply only if smaller than 10
+        {
+        second_digit += digit * 2;
+        card *= 0.10;
+        }
     }
-    int card = 40330201;
-    int modulo_a = 10;
-    int second_digit_1 = card % modulo_a;
-    int modulo_b = modulo_a * 100;
-    int second_digit_2 = card % modulo_b;
-    int modulo_c = modulo_b * 100;
-    int second_digit_3 = card % modulo_c;
-    int every_first_digit = 0;
-    int every_second_digit = 0;
-
-
+    int result = (first_digit + second_digit) % 10; // Check if a card is valid by Luhn's Algorithm
+    while (user_card != 0)
+    {
+            if (result == 0)
+     {
+         printf("Valid card.\n");
+     }
+    else
+      {
+         printf("Invalid card.\n");
+      }
+      break;
+    }
+    unsigned long x = user_card;
+    if (result == 0) // Check beginning numbers to determine the card provider
+    {
+        do
         {
-            while(second_digit_2 > 10)
+            x *= 0.10;
+            if ((user_card > american_express_15 && x == 37) || x == 34)
             {
-                second_digit_2 *= 0.10;
+                printf("American Express");
             }
+            else if ((user_card > master_card_or_visa_16 && x == 51) || x == 52 || x == 53 || x == 54 || x == 55)
+            {
+                printf("Master Card\n");
+            }
+
         }
-
-
+        while(x > 99);
+        x *= 0.10;
+        if (((user_card > master_card_or_visa_16 || user_card > visa_13) && x == 4) || x == 5)
+            {
+                printf("Visa\n");
+            }
+    }
+    else if (user_card == 0)
         {
-            while(second_digit_3 > 10)
-            {
-                second_digit_3 *= 0.10;
-            }
+        printf("Invalid card.\n");
         }
-
-
-
-    int a = 40330201;
-    int b = a % 10;
-    a -= b;
-
-    printf ("!    %d        %d            %d              !", second_digit_1, second_digit_2, second_digit_3);
-
-
-
-    return x;
-
 }
+
